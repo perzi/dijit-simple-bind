6f554af40658bbfbf581d97faccb9a86baf091c6
Per Zimmerman <per.zimmerman@gmail.com>
2014-11-16 13:50:03 +0100

    Removed templateString tests

--- test/_SimpleBindMixinSpec.js
+++ test/_SimpleBindMixinSpec.js
@@ -12,55 +12,6 @@ define([
     _SimpleBindMixin
 ) {
 
-    describe("templateString namespacing", function() {
-
-        var cls;
-        var CLS = declare([_WidgetBase, _TemplatedMixin, _SimpleBindMixin], {
-            A: "X",
-            B: "Y"
-        });
-
-        afterEach(function() {
-            cls.destroy();
-        });
-
-        it("Should transform templateString for attributes", function() {
-            cls = new CLS({ templateString: '<div class="{{A}}"></div>' });
-            expect(cls.templateString).toBe('<div class="{{1:A}}"></div>');
-        });
-
-        it("Should transform templateString for textnodes", function() {
-            cls = new CLS({ templateString: '<div>{{A}}</div>' });
-            expect(cls.templateString).toBe('<div>{{2:A}}</div>');
-        });
-
-        it("Should increse counter for namespacing", function() {
-            cls = new CLS({ templateString: '<div>{{A}}</div>' });
-            expect(cls.templateString).toBe('<div>{{3:A}}</div>');
-        });
-
-        it("Should transform templateString for multiple  textnodes", function() {
-            cls = new CLS({ templateString: '<div>{{A}} {{A}}</div>' });
-            expect(cls.templateString).toBe('<div>{{4:A}} {{4:A}}</div>');
-        });
-
-        it("Should transform templateString multiple attributes", function() {
-            cls = new CLS({ templateString: '<div class="{{A}} {{A}}"></div>' });
-            expect(cls.templateString).toBe('<div class="{{5:A}} {{5:A}}"></div>');
-        });
-
-        it("Should transform templateString for all properties", function() {
-            cls = new CLS({ templateString: '<div class="{{A}} {{B}}">{{B}} {{A}}</div>' });
-            expect(cls.templateString).toBe('<div class="{{6:A}} {{6:B}}">{{6:B}} {{6:A}}</div>');
-        });
-
-        it("Should accept whitespace around property name", function() {
-            cls = new CLS({ templateString: '<div class="{{ A }} {{ B }}">{{ B }} {{ A }}</div>' });
-            expect(cls.templateString).toBe('<div class="{{7:A}} {{7:B}}">{{7:B}} {{7:A}}</div>');
-        });
-
-    });
-
     describe("Bindable Attributes", function() {
 
         var cls;
